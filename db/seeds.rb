3a9f8723ca05217f23b29f2d239eb807cf7ce961
chepyego <jchepyego@gmail.com>
2020-03-18 11:25:32 +0300

    validation!

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,22 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Product.delete_all
-
-
-Product.create!(title: 'Portable keyboards',
-description: %{<p>portable keyboard,brand new, with one year warranty</p>},
-
-image_url: '3.jpg',
-price: 500)
+# . . .
+Product.create!(title: 'Wireless keyboard',
+description:
+%{<p>
+<em>Responsive, Intuitive Interfaces with Ruby</em>
+The next step in the evolution of user interfaces is here.
+Chatbots let your users interact with your service in their
+own natural language. Use free and open source tools along
+with Ruby to build creative, useful, and unexpected interactions
+for users. Take advantage of the Lita framework’s step-by-step
+implementation strategy to simplify bot development and testing.
+From novices to experts, chatbots are an area in which everyone
+can participate. Exercise your creativity by creating chatbot
+skills for communicating, information, and fun.
+</p>},
+image_url: 'synix.jpg',
+price: 20.00)
+# . . .
 
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,53 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+fixtures :products
+
   # test "the truth" do
   #   assert true
   # end
+  test "products attributes must not be empty" do
+    product = Product.new 
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+  test "product price must be positive" do
+   product = Product.new(title: "wireles keyboard", description:"yyyyy", image_url:"synix.jpg")
+   
+   product.price = -1
+   assert product.invalid?
+   assert_equal ["must be greater than or equal to 0.01"],
+     product.errors[:price]
+
+   product.price = 0
+   assert product.invalid?
+   assert_equal["must be greater than or equal to 0.01"]
+     product.errors[:price]
+
+   product.price = 1
+   assert product.valid?
+end
+def new_product(image_url)
+  Product.new(title:   "wireless keyboard", description: "yyyyy", price: 12, image_url: image_url)
+end
+test "image_url" do
+  ok = %w{fred.gif fred.jpg fred.png  FRED.JPG FRED.jpg http://a.b.c/x/y/z/fred.gif }
+  bad = %w{ fred.doc fred.gif/more fred.gif.more }
+  ok.each do |image_url|
+    assert new_product(image_url).valid?, "#{image_url} shouldn't be invalid"
+  end
+  bad.each do |image_url|
+    assert new_product(image_url).invalid?, "#{image_url} shouldn't be valid"
+  end
+
+end
+test "product is not valid without a unique title -i18n" do
+  product = Product.new(title:     products(:synix).title, description: "hhhhhhhhhhhh", price: 1, image_url: "synix.jpg")
+  assert product.invalid?
+  assert_equal [I18n.translate('errors.messages.taken')], product.errors[:title]
+end
+
 end
