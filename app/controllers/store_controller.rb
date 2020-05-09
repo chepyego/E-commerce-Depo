9dcb1e46a47c6b17bf2516b275d9bdf326b1dbcd
chepyego <jchepyego@gmail.com>
2020-05-09 15:46:50 +0300

    cart creation

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,16 @@
 class StoreController < ApplicationController
+  def increament_count 
+    if session[:counter].nil?
+      session[:counter] = 0
+    end
+    session[:counter] = +1
+
+  end
+
   def index
+    @count = increament_count
     @products = Product.order(:title)
+    @cart = CurrentCart
+    @message = "You have been here #{@count}times " if increament_count > 5
   end
 end
